5f0137f7b25e907246ff241ce57af9d68222f1e8
Taiki Okano <taiki@okano.tokyo>
2023-11-04 15:24:34 +0900

    Set up Makefile with Docker
    - Execute `make docker docker-run` then runs the server on 5000

---
+++ chat/server/src/server.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "chat.h"
+
+#define BUFF_LEN 1024
+
+void display_help() {
+    fprintf(stdout, "Usage: server [port]\n");
+}
+
+int main(int argc, char* argv[]) {
+
+    if(argc != 2) {
+        display_help();
+    }
+
+    int port = atoi(argv[1]);
+
+    if(port == 0) {
+        char err_msg[BUFF_LEN];
+        snprintf(err_msg, sizeof(err_msg), "Cound not convert to a port number: %s", argv[1]);
+        perror(err_msg);
+    }
+
+    if(port < 1 || port > 65535) {
+        printf("Invalid port: %d\n", port);
+    }
+
+    start_chat_server(&port);
+    return 0;
+}
