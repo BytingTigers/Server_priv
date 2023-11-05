91caa8cdb4e3a5225b72ee0c2cf6425564918c32
hokuma <ho7221@naver.com>
2023-11-05 12:29:37 +0000

    verify_jwt fixed

--- server/chat/src/verify_jwt.c
+++ server/chat/src/verify_jwt.c
@@ -8,8 +8,9 @@ int verify_jwt(const char *jwt_string, const char *username) {
 
     jwt_t *jwt = NULL;
 
-    // int ret = jwt_decode(&jwt, jwt_string, (const unsigned char *)SECRET_KEY, SECRET_KEY_LEN);
-    int ret = jwt_decode(&jwt, jwt_string, NULL, 0);
+    char *key = strdup(SECRET_KEY);
+    int ret = jwt_decode(&jwt, jwt_string, key, strlen(key));
+    // int ret = jwt_decode(&jwt, jwt_string, NULL, 0);
 
     if (ret != 0) {
         fprintf(stderr, "Invalid JWT.\n");
